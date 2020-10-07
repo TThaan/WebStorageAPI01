1ccf2539eba953b30795975bfc643f0817ecddfb
TThaan <tthaan@gmx.de>
2020-10-07 13:35:08 +0200

    completed version 1
    Simple example of using the  WepStorageAPI coarsely following the introduction on: https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Client-side_web_APIs/Client-side_storage

---
+++ WebStorageAPI01Solution/WebStorageAPI01/Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebStorageAPI01.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ WebStorageAPI01Solution/WebStorageAPI01/wwwroot/js/site.js
@@ -0,0 +1,53 @@
+﻿// source:
+// https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Client-side_web_APIs/Client-side_storage
+
+// All needed elements are defined as constants at script start.
+const form = document.querySelector('form');
+const nameInput = document.querySelector('#entername');
+const submitBtn = document.querySelector('#submitname');
+const forgetBtn = document.querySelector('#forgetname');
+
+const h1 = document.querySelector('h1');
+const personalizedParagraph = document.querySelector('.personalizedParagraph');
+
+// If you don't want an automatic page reload when pressing 'Enter' or 'Forget'
+// include the next part.
+
+// form.addEventListener('submit', function (e) 
+// {
+//     e.preventDefault();
+//     alert("Submit btn's default behavior deactivated.");
+// });
+
+// This function runs when the 'Hello' button is clicked.
+submitBtn.addEventListener('click', function ()
+{
+    localStorage.setItem('name', nameInput.value);
+});
+
+// This function runs when the 'Forget' button is clicked.
+forgetBtn.addEventListener('click', function ()
+{
+    localStorage.removeItem('name');
+});
+
+// This function is called 'onload'.
+function nameDisplayCheck()
+{
+    // A personalized view is shown when the key 'name' in the localStorage has a value.
+    if (localStorage.getItem('name'))
+    {
+        h1.textContent = 'Welcome, ' + localStorage.getItem('name');
+        personalizedParagraph.textContent = 'This page is for you, ' + localStorage.getItem('name') + '! ;) ';
+    }
+    // A general view is shown otherwise.
+    else
+    {
+        h1.textContent = 'Welcome';
+        personalizedParagraph.textContent = 'This page is public!';
+    }
+}
+
+// The nameDisplayCheck() function runs every time the page is loaded.
+// Otherwise the personalized or general view isn't loaded across page reloads.
+document.body.onload = nameDisplayCheck;
